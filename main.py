ba1d6390fa5b63ffbe2507c7903fe31d8f3c664e
Dev Kumar Verma <43582323+Shijokingo@users.noreply.github.com>
2021-11-30 17:05:23 +0530

    Create main.py
    Python Entry Level Programmer Certification Handson

---
+++ main.py
@@ -0,0 +1,27 @@
+# Hello World program in Python
+    
+print ("Hello World!");
+print ("Hello", "Future", "Python", "Programmer!")
+print ("Hello!", end="");\
+print ("\nHello python is a great language")
+print ("Hello", "Future", "Python", "Programmer!", sep="-")
+print ("Hello", "Future", "Python", "Programmer!", sep="#")
+print("Hello \"Python\" world")
+print(1, 2, 3, 4, sep='#', end='&');
+print("\nHello" + " " "future" + " " "python!\n")
+print("Literals")
+print(type(10))
+print(type(45.50))
+print("Operators")
+print("Arithmetic Operator -**- Exponential")
+
+print(2 ** 3)
+print(2. ** 3.)
+print(2 ** 3.)
+print(2. ** 3)
+
+print("Arithmetic Operator -*- Multiplication")
+print(2 * 3)
+print(2.* 3.)
+print(2 * 3.)
+print(2.* 3)
