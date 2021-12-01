bd969dbe03fae107d26418de889e4ad240416140
Dev Kumar Verma <43582323+Shijokingo@users.noreply.github.com>
2021-12-01 11:11:28 +0530

    Create seceretnumber.py
    No Game using Python

---
+++ seceretnumber.py
@@ -0,0 +1,7 @@
+#Secret No Game using Python
+secret_number = 3
+guess = int (input("Guess a number: "))
+while guess != secret_number:
+    guess = int (input("Guess a number: "))
+else 
+    print ("Congratulations you Guessed it right")
